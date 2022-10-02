4cdea10b0383dc285fa3a29d8445c37d77d9256a
SobhanAshrafi <74056968+SobhanAshrafi@users.noreply.github.com>
2022-10-02 20:12:35 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+
+import random
+
+from PIL import Image
+
+random.seed()
+
+# generate 3 random numbers to creat a random rgb color
+random_color_generator = lambda : ( random.randrange(0,255), random.randrange(0,225), random.randrange(0,255) )
+ 
+
+print(__name__)
+if __name__ == '__main__':
+
+    # creat new image
+    im = Image.new("RGB",(1024,1024),(255,255,255))
+
+    for j in range(im.height):
+        
+        Color = random_color_generator()
+        
+        for i in range(im.width):
+            
+            # uncomment below line to make all pixels color random
+            # Color = random_color_generator()
+            
+            im.putpixel((i,j),Color)
+            
+
+    # to show generated image
+    im.show()
+
+
+
+    # save result image
+    im.save("result/img.jpg")
+    im.close()
+
+
